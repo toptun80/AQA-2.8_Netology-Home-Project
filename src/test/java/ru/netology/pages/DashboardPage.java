71fcf2b413da816870d63fa2f22b9a43a8700175
Michael Urusov <toptun80@mail.ru>
2020-03-07 23:50:29 +0300

    fixed DasboardPage.java and updated Перевод денег с карты на карту.feature

--- src/test/java/ru/netology/pages/DashboardPage.java
+++ src/test/java/ru/netology/pages/DashboardPage.java
@@ -1,28 +1,18 @@
 package ru.netology.pages;
 
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import ru.alfabank.alfatest.cucumber.annotations.Name;
 import ru.alfabank.alfatest.cucumber.api.AkitaPage;
 
-import java.util.List;
-
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-
 @Name("Личный кабинет")
 public class DashboardPage extends AkitaPage {
 
-    private String card1Info = $(withText("**** 0001")).getText();
-    private String sumInfo = card1Info.substring(card1Info.indexOf("баланс:") + 7, card1Info.lastIndexOf("р.")).trim();
-
-    @Name("Баланс карты")
-    public int balance = Integer.parseInt(sumInfo);
+    @Name("Информация о карте")
+    @FindBy(css = ".list__item")
+    public SelenideElement cardInfo;
 
     @Name("Пополнить")
-    @FindAll({
-            @FindBy(css = "[data-test-id=action-deposit]")
-    } )
-    public List<SelenideElement> replenishButtons;
+    @FindBy(css = "[data-test-id=action-deposit]")
+    public SelenideElement replenishButtons;
 }
